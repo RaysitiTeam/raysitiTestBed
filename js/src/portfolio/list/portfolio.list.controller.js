e37241afd92aab78e71fb7e0c73aaee1fb5ca215
avj2352 <avj2352@gmail.com>
2017-02-24 19:47:57 +0530

    Issue with UI Grid

--- js/src/portfolio/list/portfolio.list.controller.js
+++ js/src/portfolio/list/portfolio.list.controller.js
@@ -5,28 +5,36 @@
     function PortfolioListController($scope, PortfolioListService) {
         var vm = $scope;
         vm.allListItems = [];
-        vm.allListItems = PortfolioListService.getAllListItems(); //get from Service
-        vm.portfolioList = [
-            {
-                "name": "Ninnu Project",
-                "category": "Logo",
-                "description": "This is a simple Description"
-            },
-            {
-                "name": "Ninnu Project",
-                "category": "Logo",
-                "description": "This is a simple Description"
-            },
-            {
-                "name": "Ninnu Project",
-                "category": "Logo",
-                "description": "This is a simple Description"
-            },
-            {
-                "name": "Ninnu Project",
-                "category": "Logo",
-                "description": "This is a simple Description"
-            }
-        ];
+        vm.portFolioListOptions = {            
+            enableSorting: true,
+            columnDefs: [
+                { name: 'Portfolio', field: 'name' },
+                { name: 'Category', field: 'category' },
+                { name: 'Client', field: 'client' }
+            ],
+        };
+        var getAllListItemsService = PortfolioListService.getAllListItems(); //get from Service
+        getAllListItemsService
+            .then(function onSuccess(response) {
+                // Handle success
+                var data = response.data;
+                var status = response.status;
+                var statusText = response.statusText;
+                var headers = response.headers;
+                var config = response.config;
+                //NOTE: assigning data to ui-table 
+                vm.portFolioListOptions.data.push(data);
+                console.log('Table object is: ', data);
+            }).catch(function onError(response) {
+                // Handle error
+                var data = response.data;
+                var status = response.status;
+                var statusText = response.statusText;
+                var headers = response.headers;
+                var config = response.config;
+                console.error('Error retrieving service-getAllListItems', status);
+            });
+            // $scope.$apply();
+
     }//end:PortfolioListController
 }());//iife
--- js/src/portfolio/list/portfolio.list.service.js
+++ js/src/portfolio/list/portfolio.list.service.js
@@ -1,19 +1,15 @@
-(function(){
+(function () {
     angular.module('raysiti')
-    .factory('PortfolioListService',PortfolioListService);
+        .factory('PortfolioListService', PortfolioListService);
     PortfolioListService.$inject = ['$http'];
-    function PortfolioListService($http){
+    function PortfolioListService($http) {
         return {
-            getAllListItems:getAllListItems
+            getAllListItems: getAllListItems
         };//end:return
-        function getAllListItems(){
-            var promise = $http.get('server/api/listPortfolio.php')
-                .success(function(data,status,headers,config){
-                    return data;
-                }).error(function(data,status,headers,config){
-                    console.error('Error getting response - getAllListItems',data);
-                });//end:success/error
+        function getAllListItems() {
+            var promise = $http.get("server/api/listPortfolio.php");
+            //NOTE: New to Angular 1.6.x                        
             return promise;
-        }//end:getAllListItems
-    }//end:PortfolioListService
+    }//end:getAllListItems
+}//end:PortfolioListService
 }());//iife
