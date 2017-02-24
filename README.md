d3570fe6419d2e8746ab8f3de325e2f8b4825d17
avj2352 <avj2352@gmail.com>
2017-02-24 12:52:01 +0530

    initialized with angular

--- README.md
+++ README.md
@@ -2,7 +2,9 @@
 
 ## Progress Track 
 
-### 24th Feb 2017
+### 24th Feb 2017 - 12:51 pm
 
 - Initialized and Uploaded on https://github.com/RaysitiTeam/raysitiTestBed.git
+- Initialized with Angular JS
 
+### 24th Feb 2017 -
---
+++ js/src/app.js
@@ -0,0 +1,15 @@
+(function(){
+    //Module declaration - raysiti
+    angular.module('raysiti',[
+        //angular dependencies
+        'ngAnimate',
+        //third party dependencies
+        'ui.router'
+    ]);
+
+    angular.module('raysiti').run(    
+    function init($rootScope){
+        $rootScope.appName = "RAYSITI: 3D Animation | 2D Animation | Architectural Walktroughs | Website Design | Logo Design | Brochure Design | Video Production | Animated Videos | Branding Solutions";
+    });//end:run
+
+}());//iife
